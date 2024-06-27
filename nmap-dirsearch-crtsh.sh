9a1e7ebed946826941f517e119a5770e0e615971
tobiasGuta <156011774+tobiasGuta@users.noreply.github.com>
2024-06-26 22:11:01 -0400

    Update and rename nmap-dirsearch.sh to nmap-dirsearch-crtsh.sh

--- nmap-dirsearch-crtsh.sh
+++ nmap-dirsearch-crtsh.sh
@@ -2,7 +2,7 @@
 
 # Check if at least two arguments are provided
 if [ "$#" -lt 2 ]; then
-    echo "Usage: $0 <domain> <nmap-only|directory-only|both> [additional-options]"
+    echo "Usage: $0 <domain> <nmap-only|directory-only|crtsh-only|all>"
     exit 1
 fi
 
@@ -27,14 +27,24 @@ case $OPTION in
         dirsearch -u "$DOMAIN" $EXTRA_ARGS -o "$Directory/dirsearch"
         echo "The results of dirsearch are stored in $Directory/dirsearch"
         ;;
-    both)
+    crtsh-only)
+        echo "Enumerating subdomains with crt.sh..."
+        crtsh_enum=$(curl -s "https://crt.sh/?q=%25.$DOMAIN&output=json" | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u)
+        echo "$crtsh_enum" > "$Directory/crtsh_subdomains"
+        echo "Crt.sh enumeration results saved in $Directory/crtsh_subdomains."
+        ;;
+    all)
         nmap $EXTRA_ARGS "$DOMAIN" > "$Directory/nmap"
         echo "The results of nmap are stored in $Directory/nmap."
         dirsearch -u "$DOMAIN" $EXTRA_ARGS -o "$Directory/dirsearch"
         echo "The results of dirsearch are stored in $Directory/dirsearch"
+        echo "Enumerating subdomains with crt.sh..."
+        crtsh_enum=$(curl -s "https://crt.sh/?q=%25.$DOMAIN&output=json" | jq -r '.[].name_value' | sed 's/\*\.//g' | sort -u)
+        echo "$crtsh_enum" > "$Directory/crtsh_subdomains"
+        echo "Crt.sh enumeration results saved in $Directory/crtsh_subdomains."
         ;;
     *)
-        echo "Invalid option. Use 'nmap-only', 'directory-only', or 'both'."
+        echo "Invalid option. Use 'nmap-only', 'directory-only', 'crtsh-only', or 'all'."
         exit 1
         ;;
 esac
